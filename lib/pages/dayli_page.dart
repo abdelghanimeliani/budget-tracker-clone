7743b5e71e01926d6032c69df08858a6e3106db7
Abdelghani Meliani <a.meliani@esi-sba.dz>
2021-03-27 21:43:59 +0100

    days barre

--- lib/pages/dayli_page.dart
+++ lib/pages/dayli_page.dart
@@ -1,5 +1,7 @@
+import 'package:budget_tracker_ui/json/day_month.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import '../theme/colors.dart';
 
 class DailyPage extends StatefulWidget {
@@ -8,6 +10,7 @@ class DailyPage extends StatefulWidget {
 }
 
 class _DailyPageState extends State<DailyPage> {
+  int activeDay = 3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +38,74 @@ class _DailyPageState extends State<DailyPage> {
               padding: const EdgeInsets.only(
                   top: 60, right: 20, left: 20, bottom: 25),
               // ta3 el padding
-              child: Column(),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Daily Transaction",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: black),
+                      ),
+                      Icon(AntDesign.search1)
+                    ],
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: List.generate(days.length, (index) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              activeDay = index;
+                            });
+                          },
+                          child: Container(
+                            width: ((size.width - 40) / 7),
+                            child: Column(
+                              children: [
+                                Text(
+                                  days[index]["label"],
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Container(
+                                  width: 30,
+                                  height: 30,
+                                  decoration: BoxDecoration(
+                                      color: activeDay == index
+                                          ? primary
+                                          : Colors.transparent,
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                          color: activeDay == index
+                                              ? primary
+                                              : black.withOpacity(0.1))),
+                                  child: Center(
+                                    child: Text(
+                                      days[index]["day"],
+                                      style: TextStyle(
+                                          color: activeDay == index
+                                              ? white
+                                              : black.withOpacity(0.1),
+                                          fontSize: 10),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      }))
+                ],
+              ),
             )),
       ]),
     );
